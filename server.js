9756c9480fc34ea7b60c38323fb3686157a4d64b
Ayush Vattoli <ayush.vatts@gmail.com>
2025-08-22 04:49:18 -0700

    update audio file paths for static serving

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const app = express();
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
 app.use(express.static('public'));
+app.use('/songs', express.static('songs'));
 
 const io = new Server(server);
 
